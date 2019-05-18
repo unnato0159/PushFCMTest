3c965db693f791615b9dee83179fe8080da042c0
unnato0159 <unnato01@naver.com>
2019-05-18 14:17:20 +0900

    구버전에서 알림 띄우는 코드 작성

--- app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
+++ app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
@@ -6,11 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.tje.pushfcmtest.MainActivity;
+import com.tje.pushfcmtest.R;
 
 public class MyMessageService extends FirebaseMessagingService {
 
@@ -52,6 +55,43 @@ public class MyMessageService extends FirebaseMessagingService {
         // 알림을 띄우는 역할을 담당하는 매니저를 안드로이드 시스템으로부터 얻어옴
         // 안드로이드 시스템 서비스는 여러가지의 종류를 내포 , 이번에 쓰는건 알림이라고 반드시 강제 캐스팅 .
         NotificationManager notificationManager =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //알림 내용을 구성해주는 Builder  변수 생성
+        NotificationCompat.Builder builder = null;
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O) {
+         // 안드로이드 버젼 코드가  0 보다 크다 ! 오레오 이상의 최신버전이다.
+            // 알림 채널을 만들어서 해당 채널에 푸시알림을 던져주는 방식
+        }
+        else {
+             //오레오 보다는 밑의 버전이다 . N 버전이하 누가 마쉬멜로우 롤리팝 아이스크림샌드위치
+            // 알림 채널이라는 기능이 없어서 그냥 통일해서 쓰면 앱이 죽어버림.
+
+            builder = new NotificationCompat.Builder(this);
+            //푸시알림 상태창에 뜨는 아이콘 설정
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            //알림의 제목 설정
+            builder.setContentTitle(title);
+            //알림의 내용 설정
+            builder.setContentText(content);
+            //알림의 울리는 설정
+            builder.setSound(defaultNotiUri);
+            // 알림이 왔을때 울리는 진동 패턴
+            builder.setVibrate(new long[]{1,1000});
+            // 자동삭제 되도록 할건지
+            builder.setAutoCancel(true);
+            // 이 알림을 누르면 어디로 갈지? 아까 만든 pending intent활용 지정
+            builder.setContentIntent(pendingIntent);
+            // 실제로 알림을 띄우는 부분
+
+            // id 를 일반 숫자로 고정하면, 항상 같은 id 가 대입. => 여러번 알림이 오면 기존 알림을 덮어씀 .
+            //만약 여러개의 알림을 모두 띄우고 싶다면 그때그때 다른 숫자가 들어가도록 코딩
+            notificationManager.notify(1,builder.build());
+
+
+
+        }
+
 
 
     }
