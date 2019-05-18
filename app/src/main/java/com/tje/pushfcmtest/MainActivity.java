92cb80df05619fb1efac5a2f393d7e8a04c04861
unnato0159 <unnato01@naver.com>
2019-05-18 13:48:28 +0900

    기본 푸시 알림 받기 테스트

--- app/src/main/java/com/tje/pushfcmtest/MainActivity.java
+++ app/src/main/java/com/tje/pushfcmtest/MainActivity.java
@@ -1,7 +1,15 @@
 package com.tje.pushfcmtest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "FCM 등록 실패", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //  FCM 서버에 등록된 토큰 값
+                 String token =task.getResult().getToken();
+
+                Log.d("발급된 토큰",token);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
+++ app/src/main/java/com/tje/pushfcmtest/fcm/MyMessageService.java
@@ -1,8 +1,15 @@
 package com.tje.pushfcmtest.fcm;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyMessageService extends FirebaseMessagingService {
 
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
 
+        Log.d("새 토큰 발급",s);
+    }
 }
